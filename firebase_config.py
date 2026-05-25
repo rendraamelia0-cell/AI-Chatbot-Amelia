0cc4fbf71b63454bda431d44241e4cf38ead3865
rendraamelia0-cell <rendraamelia0@gmail.com>
2026-05-25 10:32:26 +0700

    Create firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBR1-nWQa3DFnHezhKwZp70Lt8nWpga4K4",
+  authDomain: "mell-meows.firebaseapp.com",
+  projectId: "mell-meows",
+  storageBucket: "mell-meows.firebasestorage.app",
+  messagingSenderId: "753189004249",
+  appId: "1:753189004249:web:3c97c069feaa2e19b5afc8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
